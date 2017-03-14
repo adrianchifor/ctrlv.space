3f736f2bc6f752bee6d8767cf4a4a90c937e8729
Adrian Chifor <chifor.adrian1993@gmail.com>
2017-03-14 00:31:54 +0000

    Refactoring. Better validation. Added self-destruct.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, request, render_template
-from functools import wraps
+from flask import Flask, jsonify, request, render_template, make_response
 from redis import StrictRedis
-import os, hashlib, uuid
+import os, hashlib, uuid, logging
 
 get_hash = lambda: hashlib.md5(uuid.uuid4().bytes).hexdigest()[:12]
 
@@ -9,39 +8,50 @@ app = Flask(__name__)
 
 db = StrictRedis.from_url(os.environ['REDIS_URL'], db=2, decode_responses=True)
 
-def json(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        return jsonify(f(*args, **kwargs))
-    return wrapper
+@app.before_first_request
+def setup_logging():
+    app.logger.addHandler(logging.StreamHandler())
+    app.logger.setLevel(logging.INFO)
+
 
 @app.route('/api/v1/create', methods=['POST'])
-@json
 def api_v1_create():
     if 'ciphertext' not in request.form:
-        return {
-            'error': 'Bad Request'
-        }
+        return make_response(jsonify(error='Bad Request'), 400)
+
+    ciphertext = request.form['ciphertext']
+
+    if (not isinstance(ciphertext, str)) or (len(ciphertext) > 5100) or (len(ciphertext) == 0):
+        return make_response(jsonify(error='Bad Request'), 400)
 
     key = get_hash()
-    db.set(key, request.form['ciphertext'])
+    db.set(key, ciphertext)
 
-    return {
-        'error': None,
-        'key': key
-    }
+    if 'destroy' in request.form:
+        if request.form['destroy'] == "true":
+            db.set(key + "_destroy", True)
+
+    return make_response(jsonify(error=None, key=key), 200)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/<key>")
 def paste(key):
     if not db.exists(key):
-        return render_template("404.html")
+        return render_template("404.html"), 404
 
     ciphertext = db.get(key)
+
+    destroy_key = key + "_destroy"
+    if db.exists(destroy_key):
+        db.delete(key, destroy_key)
+
     return render_template("paste.html", ciphertext=str(ciphertext))
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
